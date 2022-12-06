8b3b34d8d6ad02f29fe95876cb12e72f90a48706
RoshanAli339 <roshanali339lps@gmail.com>
2022-12-06 18:29:22 +0530

    Tuesday 06 December 2022 06:29:22 PM IST

--- 2c.php
+++ 2c.php
@@ -1,8 +1,19 @@
 <?php
+	function myStrlen($str)
+	{
+		$i = 0;
+		while ($str != '')
+		{
+			$str = substr($str, 1);
+			$i++;
+		}
+		return $i;
+	} 
+
 	function wordCount($sen)
 	{	
 		$count = 0;
-		for ($i = 0; $i < strlen($sen); $i++)
+		for ($i = 0; $i < myStrlen($sen); $i++)
 		{
 			if ($sen[$i] == ' ' || $i == strlen($sen)-1)
 				$count++;
@@ -10,6 +21,8 @@
 		return $count;
 	}
 	$a = "Hello world Welcome to Skill Oriented Course PHP Personal Home Page";
+	$length = myStrlen($a);
+	echo "Length of given string: $length<br>";
 	$words = wordCount($a);
 	echo "No.of words: $words<br>";
 ?>
